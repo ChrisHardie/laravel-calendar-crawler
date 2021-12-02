4e26982efec3a0f3f1085a55c8a554ff38a122cc
Chris Hardie <chris@chrishardie.com>
2021-12-01 19:50:42 -0500

    Respect a feed's frequency in the checkable scope, make frequency required in DB, correct faulty logic

--- src/Models/CalendarSource.php
+++ src/Models/CalendarSource.php
@@ -44,27 +44,18 @@ class CalendarSource extends Model
      * @param  string  $frequency
      * @return mixed
      */
-    public function scopeCheckable(Builder $query, string $frequency = '')
+    public function scopeCheckable(Builder $query)
     {
-        // Set default frequency to Source-specific or configured amount
-        $check_frequency_minutes = ! empty($this->frequency)
-            ? (int) $this->frequency
-            : config('calendar-crawler.default_update_frequency');
-
         $query
             ->where('active', true)
             ->where('admin_pause_checks', false);
 
-        $query->where(function ($query) use ($check_frequency_minutes) {
+        $query->where(function ($query) {
             $query
                 // Never been checked
                 ->whereNull('last_check_at')
                 // Haven't been checked in the last X minutes given sitewide update frequency
-                ->orWhere(
-                    'last_check_at',
-                    '<',
-                    Carbon::now()->subMinutes($check_frequency_minutes)
-                );
+                ->orWhereRaw('last_check_at < DATE_SUB(now(), INTERVAL frequency MINUTE)');
         });
 
         // Sources where no next check is set or where it has passed.
